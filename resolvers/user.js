83052790cff9481d7aa8dbc2c6264f5f24559319
Alexander Frederik Vreeken <alexanderfrederikvreeken@MacBook-Air-13-Sander-Vreeken.local>
2021-03-25 18:47:06 +0100

    Added the UserInput and password encryption.

--- resolvers/user.js
+++ resolvers/user.js
@@ -1,17 +1,16 @@
+const bcrypt = require('bcryptjs')
+
 const defineDate = require('../functions/defineDate')
 const setCookie = require('../functions/setCookie')
 const UserM = require('../models/User.model')
 
 const UserResolvers = {
     Mutation: {
-        async createUser(parent, args, context) {
-            const user = {
-                email: 'info@sandervreeken.com',
-                name: 'SanderVreeken',
-                password: 'Rosebud3',
-            }
-
-            const response = await UserM.create(user)
+        async createUser(parent, { user }, context) {
+            user.password = await bcrypt.hash(user.password, 12)
+            const response = await new UserM(user).save()
+            // console.log(response)
+            
             setCookie(context, defineDate(), 'token')
 
             return true
